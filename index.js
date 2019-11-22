27016a61789548f6fcf4d892e75381218bedf46e
Eneas <eneasmarin@iMac-de-Biome.local>
2019-11-22 17:32:21 +0100

    new

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const ytdl = require('ytdl-core');
+const app = express();
+var path = require('path');
+
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname, 'dist/index.html'));
+  });
+  
+app.use('/', express.static(path.join(__dirname, 'dist')));
+
+
+
+app.listen(4000, () => {
+    console.log('Server Works !!! At port 4000');
+});
+
+app.get('/downloadmp3', (req,res) => {
+	var url = req.query.url;
+	res.header('Content-Disposition', 'attachment; filename="audio.mp3"');
+	ytdl(url, {
+		format: 'mp3',
+		filter: 'audioonly'
+	}).pipe(res);
+});
+
+app.get('/downloadmp4', (req,res) => {
+	var url = req.query.url;
+	res.header('Content-Disposition', 'attachment; filename="video.mp4"');
+	ytdl(url, {
+		format: 'mp4'
+	}).pipe(res);
+});
